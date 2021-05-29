be891e7187d1de07ff9ceecdfb37f2d4edbef111
Uriel <urielbenitez96@hotmail.com>
2021-05-28 23:27:34 -0500

    cambio de pierto

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 2000),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '8dcb056b5dd3d3f7a025435997e30665'),
+      secret: env("ADMIN_JWT_SECRET", "8dcb056b5dd3d3f7a025435997e30665"),
     },
   },
 });
